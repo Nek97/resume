2050ac7f47d52151ddbbabebe71471caea0790df
Nek97 <tortora.anielllo.ta@gmail.com>
2020-07-29 12:50:11 +0200

    0.0.0154

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
               <li><a href="https://www.facebook.com/nello1997"><i className="fa fa-facebook" /></a></li>
               <li><a href="https://wa.me/+393457086780"><i className="fa fa-whatsapp" /></a></li>
               <li><a href="https://github.com/Nek97"><i className="fa fa-github" /></a></li>
-              <li><a href="www.linkedin.com/in/DevNek97"><i className="fa fa-linkedin" /></a></li>
+              <li><a href="https://www.linkedin.com/in/devnek97/"><i className="fa fa-linkedin" /></a></li>
               <li><a href="https://www.instagram.com/nek97/"><i className="fa fa-instagram" /></a></li>
             </ul>
             <ul className="copyright">
